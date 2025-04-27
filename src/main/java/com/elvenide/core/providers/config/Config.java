4b31fd9357356f120b2b73471ccc3b380fd39671
Cannicide <msffodomainer@gmail.com>
2025-04-27 01:40:21 -0400

    Added saving and removing methods to Config

--- src/main/java/com/elvenide/core/providers/config/Config.java
+++ src/main/java/com/elvenide/core/providers/config/Config.java
@@ -24,6 +24,10 @@ public class Config extends YamlConfiguration {
         reload();
     }
 
+    /**
+     * Reloads the config from the file.
+     * Creates the file and its parent folders if they don't exist.
+     */
     public void reload() {
         if (!file.getParentFile().exists()) {
             boolean ignored = file.getParentFile().mkdirs();
@@ -44,6 +48,51 @@ public class Config extends YamlConfiguration {
         }
     }
 
+    /**
+     * Saves the config to the file.
+     */
+    public void save() {
+        try {
+            save(file);
+        } catch (IOException ignored) {
+            throw new RuntimeException("Failed to save config file: " + file.getAbsolutePath());
+        }
+    }
+
+    /**
+     * Sets a value and immediately saves the config.
+     * Best used when you only need to set a single value before saving.
+     * @param key String key
+     * @param value Value
+     */
+    public void setAndSave(@NotNull String key, @Nullable Object value) {
+        set(key, value);
+        save();
+    }
+
+    /**
+     * Removes a value from the config.
+     * Simply a clearer alias for <code>set(key, null)</code>.
+     * @param key String key
+     */
+    public void remove(@NotNull String key) {
+        set(key, null);
+    }
+
+    /**
+     * Removes a value and immediately saves the config.
+     * @param key String key
+     */
+    public void removeAndSave(@NotNull String key) {
+        remove(key);
+        save();
+    }
+
+    /**
+     * Gets a URI from the config.
+     * @param key String key
+     * @return URI
+     */
     public @Nullable URI getURI(@NotNull String key) {
         if (!contains(key))
             return null;
