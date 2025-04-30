3d8fd332b2303225fd5ddf540101b3b5be416a77
Cannicide <msffodomainer@gmail.com>
2025-04-30 01:51:11 -0400

    Added ConfigSupplier to automatically reload configs modularly

--- src/main/java/com/elvenide/core/plugin/CorePlugin.java
+++ src/main/java/com/elvenide/core/plugin/CorePlugin.java
@@ -2,9 +2,13 @@ package com.elvenide.core.plugin;
 
 import com.elvenide.core.ElvenideCore;
 import com.elvenide.core.events.CoreReloadEvent;
+import com.elvenide.core.providers.config.ConfigSupplier;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.ApiStatus;
+
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * ElvenideCore's powerful extension of the {@link JavaPlugin} class.
@@ -13,7 +17,8 @@ import org.jetbrains.annotations.ApiStatus;
  */
 public abstract class CorePlugin extends JavaPlugin {
 
-    private static CorePlugin instance;
+    private static CorePlugin instance = null;
+    private final HashSet<ConfigSupplier> configSuppliers = new HashSet<>();
 
     /// Only for internal use. Use {@link #onLoaded()} instead.
     @ApiStatus.OverrideOnly
@@ -53,21 +58,12 @@ public abstract class CorePlugin extends JavaPlugin {
 
     /**
      * Code run when the plugin is enabled.
-     * This method is executed before the first {@link #onReload() reload}
+     * This method is executed before the first {@link #reload() reload}
      * (Load >> Enable >> Reload).
      * @since 0.0.15
      */
     @ApiStatus.OverrideOnly
     public abstract void onEnabled();
-
-    /**
-     * Code run whenever the plugin is reloaded (including initial load).
-     * This method is first executed after the plugin is {@link #onEnabled() enabled}
-     * (Load >> Enable >> Reload).
-     * @since 0.0.15
-     */
-    @ApiStatus.OverrideOnly
-    public abstract void onReload();
 
     /**
      * Code run when the plugin is disabled.
@@ -77,23 +73,43 @@ public abstract class CorePlugin extends JavaPlugin {
     public void onDisabled() {}
 
     /**
+     * Registers one or more config suppliers.
+     * Registered config suppliers will be automatically reloaded when {@link #reload()} is called.
+     * @param suppliers Config suppliers
+     * @since 0.0.15
+     */
+    public final void registerConfigSuppliers(ConfigSupplier... suppliers) {
+        configSuppliers.addAll(List.of(suppliers));
+    }
+
+    /**
      * Registers one or more event listeners.
      * @param listeners Listeners
      * @since 0.0.15
      */
-    public static void registerListeners(Listener ...listeners) {
+    public static void registerListeners(Listener... listeners) {
+        if (instance == null)
+            throw new IllegalStateException("Plugin has not been initialized yet.");
+
         for (Listener listener : listeners)
             instance.getServer().getPluginManager().registerEvents(listener, instance);
     }
 
     /**
      * Manually reloads the plugin (e.g. to reload config files).
-     * Automatically calls {@link #onReload()} after finishing the reload.
+     * Emits a {@link CoreReloadEvent} and reloads all registered {@link #registerConfigSuppliers(ConfigSupplier...) config suppliers}.
      * @since 0.0.15
      */
     public static void reload() {
+        if (instance == null)
+            throw new IllegalStateException("Plugin has not been initialized yet.");
+
+        // Reload config suppliers
+        for (ConfigSupplier supplier : instance.configSuppliers)
+            supplier.reload();
+
+        // Emit reload event
         new CoreReloadEvent(instance).callEvent();
-        instance.onReload();
     }
 
 }
---
+++ src/main/java/com/elvenide/core/providers/config/ConfigSupplier.java
@@ -0,0 +1,33 @@
+package com.elvenide.core.providers.config;
+
+import com.elvenide.core.plugin.CorePlugin;
+
+import java.util.List;
+
+/**
+ * When registered in a CorePlugin, all configs supplied by this ConfigSupplier will automatically
+ * be reloaded when the plugin is reloaded by {@link CorePlugin#reload()}.
+ * <p>
+ * Can be registered within a CorePlugin instance using {@link CorePlugin#registerConfigSuppliers(ConfigSupplier...)}.
+ * @since 0.0.15
+ */
+public interface ConfigSupplier {
+
+    /**
+     * Gets all reloadable configs supplied by this ConfigSupplier.
+     * @return List of Config
+     * @since 0.0.15
+     */
+    List<Config> configs();
+
+    /**
+     * Manually reloads all configs supplied by this ConfigSupplier.
+     * Should not be used in most cases, as {@link CorePlugin#reload()} calls this method automatically.
+     * @since 0.0.15
+     */
+    default void reload() {
+        for (Config config : configs())
+            config.reload();
+    }
+
+}
