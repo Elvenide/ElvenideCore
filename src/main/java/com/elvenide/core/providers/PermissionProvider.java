4800d5b4ca8a6cf26d838fc8d555c07dac6ffd4e
Cannicide <msffodomainer@gmail.com>
2025-05-10 02:17:33 -0400

    Added explicit perms support to perms.has(), deprecated perms.hasExplicitly()

--- src/main/java/com/elvenide/core/providers/PermissionProvider.java
+++ src/main/java/com/elvenide/core/providers/PermissionProvider.java
@@ -3,6 +3,7 @@ package com.elvenide.core.providers;
 import com.elvenide.core.Core;
 import com.elvenide.core.Provider;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.ApiStatus;
@@ -16,14 +17,16 @@ public class PermissionProvider extends Provider {
     }
 
     /**
-     * Checks if a sender has a permission.
-     * Supports negated permissions that start with a "-", which will check if the sender doesn't have the permission.
-     * Unlike <code>has()</code>, this method will not always return true for opped/console senders -- it requires the permission
-     * to be explicitly set on the sender.
+     * Checks if a sender explicitly has a permission.
+     * <p>
+     * <i>This method is deprecated due to its previous support for negated permissions.
+     * There is no such thing as "explicitly not having" a permission; that is simply the same as "not having" it.</i>h
      * @param sender Sender (player or console)
      * @param permission Permission, optionally negated
      * @return Boolean
+     * @deprecated Use {@link #has(Permissible, String)} with the explicit-prefix ("#") at the start of the permission
      */
+    @Deprecated(since = "0.0.15", forRemoval = true)
     @Contract(pure = true)
     public boolean hasExplicitly(CommandSender sender, String permission) {
         if (permission.startsWith("-")) {
@@ -35,18 +38,48 @@ public class PermissionProvider extends Provider {
         return sender.hasPermission(permNode);
     }
 
+    @Contract(pure = true)
+    private boolean hasExplicit(Permissible sender, String permission) {
+        permission = permission.replace("\\", ""); // Remove escape characters
+        Permission permNode = new Permission(permission, PermissionDefault.FALSE);
+        return sender.hasPermission(permNode);
+    }
+
+    @Contract(pure = true)
+    private boolean hasNegated(Permissible sender, String permission) {
+        permission = permission.replace("\\", ""); // Remove escape characters
+        return !sender.hasPermission(permission);
+    }
+
     /**
      * Checks if a sender has a permission.
-     * Supports negated permissions that start with a "-", which will check if the sender doesn't have the permission.
-     * @param sender Sender (player or console)
-     * @param permission Permission, optionally negated
+     * <p>
+     * Permissions can start with the following prefixes for special behavior:
+     * <ul>
+     * <li>"-" checks if the user doesn't have the permission</li>
+     * <li>"#" checks if the user explicitly has the permission set (ignores op)</li>
+     * </ul>
+     * <p>
+     * You can use backslashes ("\") as escape characters to prevent the prefix special behavior.
+     * @param user User (player or console)
+     * @param permission Permission, with optional negated (-) and/or explicit (#) prefix
      * @return Boolean
      */
     @Contract(pure = true)
-    public boolean has(CommandSender sender, String permission) {
+    public boolean has(Permissible user, String permission) {
+        // Negated
         if (permission.startsWith("-"))
-            return !sender.hasPermission(permission.substring(1));
+            return hasNegated(user, permission.substring(1));
 
-        return sender.hasPermission(permission);
+        // Negated-Explicit (just an alias for Negated)
+        else if (permission.startsWith("#-") || permission.startsWith("-#"))
+            return hasNegated(user, permission.substring(2));
+
+        // Explicit
+        else if (permission.startsWith("#"))
+            return hasExplicit(user, permission.substring(1));
+
+        permission = permission.replace("\\", ""); // Remove escape characters
+        return user.hasPermission(permission);
     }
 }
