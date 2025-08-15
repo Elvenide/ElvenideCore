ca58198665e7d77e8c0c145922755fda933d4483
Elvenide <msffodomainer@gmail.com>
2025-08-15 19:06:14 -0400

    Deprecate tasks.builder()

--- src/main/java/com/elvenide/core/providers/task/TaskProvider.java
+++ src/main/java/com/elvenide/core/providers/task/TaskProvider.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Consumer;
+
 /**
  * This class should not be directly referenced by any plugin.
  * Its methods should only be utilized through the {@link Core#tasks} field.
@@ -19,11 +21,26 @@ public class TaskProvider extends Provider {
 
     /**
      * Returns a builder to create a new scheduled task.
+     * @deprecated All tasks must have at least one executable action.
+     *             Use {@link #create(Consumer)} instead
      * @return Task builder
      */
     @PublicAPI
     @Contract(pure = true)
+    @Deprecated(since = "0.0.17", forRemoval = true)
     public Task builder() {
         return new Task(core);
+    }
+
+    /**
+     * Creates a new scheduled task builder with the given executable action.
+     * @param consumer Consumer
+     * @return Task
+     * @since 0.0.17
+     */
+    @PublicAPI
+    @Contract(pure = true)
+    public Task create(Consumer<Task> consumer) {
+        return new Task(core).then(consumer);
     }
 }
